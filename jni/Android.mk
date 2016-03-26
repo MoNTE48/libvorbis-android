4b26897e1362772f11c9bec8d37345b89c0781f6
Maksim Gamarnik <MoNTE48@mail.ua>
2016-03-26 21:50:31 +0200

    Update

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,7 +5,6 @@ LOCAL_C_INCLUDE := $(LOCAL_PATH)/include
 include $(addprefix $(LOCAL_PATH)/, $(addsuffix /Android.mk, \
 	libogg \
 	libvorbis \
-	libvorbis-jni \
 ))
 
 
--- jni/libogg/Android.mk
+++ jni/libogg/Android.mk
@@ -3,14 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libogg
-LOCAL_CFLAGS += -I$(LOCAL_PATH)/../include -ffast-math -fsigned-char
-ifeq ($(TARGET_ARCH),arm)
-	LOCAL_CFLAGS += -march=armv6 -marm -mfloat-abi=softfp -mfpu=vfp
-endif
-
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/../include
 
 LOCAL_SRC_FILES := \
 	bitwise.c \
 	framing.c
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
