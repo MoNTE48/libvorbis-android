7b4faf3bded47d90ce5671876758dd2a619bebe8
Maksim Gamarnik <MoNTE48@mail.ua>
2016-04-02 22:50:57 +0300

    Update Android.mk

--- jni/libvorbis/Android.mk
+++ jni/libvorbis/Android.mk
@@ -9,4 +9,4 @@ LOCAL_SRC_FILES := \
 	$(addprefix ../, $(shell cd $(LOCAL_PATH)/../; \
                            find -type f -name '*.c' | \
                            grep -v psytune.c | grep -v tone.c))
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
