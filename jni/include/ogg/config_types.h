61e03f35c9ca809d7e7756235e94bd000213b894
MoNTE48 <MoNTE48@mail.ua>
2019-12-09 13:19:54 +0100

    Update Ogg and Vorbis to the latest version [https://github.com/xiph]

--- jni/include/ogg/config_types.h
+++ jni/include/ogg/config_types.h
@@ -1,7 +1,6 @@
 #ifndef __CONFIG_TYPES_H__
 #define __CONFIG_TYPES_H__
 
-/* these are filled in by configure */
 #define INCLUDE_INTTYPES_H 1
 #define INCLUDE_STDINT_H 1
 #define INCLUDE_SYS_TYPES_H 1
@@ -21,5 +20,6 @@ typedef unsigned short ogg_uint16_t;
 typedef int ogg_int32_t;
 typedef unsigned int ogg_uint32_t;
 typedef long long ogg_int64_t;
+typedef unsigned long long ogg_uint64_t;
 
 #endif
